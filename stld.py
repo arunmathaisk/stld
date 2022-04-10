0343116ad348acae89ec8174086cfbedba1d2fb2
Arun <me@arunmathaisk.in>
2022-04-10 21:35:17 +0530

    new changes

--- stld.py
+++ stld.py
@@ -9,26 +9,14 @@ LAB_NUMER = sys.argv[1]
 
 SERVER_IP = sys.argv[2]
 
-STATUS_ENDPOINT = "/status.php"
+STATUS_ENDPOINT = "/status"
 
-PAYLOAD_ENDPOINT = "/payload.php"
-
-ARGUMENT_URL = "?lab_number=" + LAB_NUMER
+ARGUMENT_URL = "/" + LAB_NUMER
 
 STATUS_URL = SERVER_IP + STATUS_ENDPOINT + ARGUMENT_URL
-
-PAYLOAD_URL = SERVER_IP + PAYLOAD_ENDPOINT + ARGUMENT_URL
 
 RESPONSE_COMMAND = requests.get(STATUS_URL)
 
 if(RESPONSE_COMMAND.text=='shutdown'):
 	os.system("/sbin/shutdown -h now")
 	os.system("/usr/sbin/shutdown -h now")
-
-if(RESPONSE_COMMAND.text=='reboot'):
-	os.system("/sbin/reboot")
-	os.system("/usr/sbin/reboot")
-
-if(RESPONSE_COMMAND.text=='payload'):
-	PAYLOAD_RESPONSE_COMMAND = requests.get(PAYLOAD_URL)
-	os.system(RESPONSE_COMMAND.text)
